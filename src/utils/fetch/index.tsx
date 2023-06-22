eca22ce8ae73838ad5da4c96c1e01309c3bab856
Tony Kosseify <tonykosseify123@gmail.com>
2023-06-22 17:59:51 +0300

    update fetch authentication

--- src/utils/fetch/index.tsx
+++ src/utils/fetch/index.tsx
@@ -1,8 +1,9 @@
-import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 const BASE_URL = process.env.BASE_URL ;
 
 
-const customFetch = (url: string, options: any) => {
+const customFetch = async (url: string, options: any) => {
   return fetch(`${BASE_URL}${url}`, {
     ...options,
     headers: {
@@ -11,6 +12,19 @@ const customFetch = (url: string, options: any) => {
       Authorization: `Bearer ${cookies().get('token')}`,
     },
   })
+  .then((res) => res.json())
+  .catch((err) => {
+    if ( err.status === 401 ) {
+      cookies().set({
+        name: 'token',
+        value: '',
+        expires: new Date('2016-10-05'),
+        path: '/', // For all paths
+      })
+      redirect('/auth/sign-in');
+    }
+    return err;  
+  });
 };
 
 
