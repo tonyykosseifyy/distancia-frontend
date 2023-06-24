db0c0d8f5b8b1182bb3e8f1ce23f037c6dbcda39
Tony Kosseify <tonykosseify123@gmail.com>
2023-06-24 05:56:13 +0300

    add delete-profile page

--- src/app/(common_layouts)/student/delete-profile/page.tsx
+++ src/app/(common_layouts)/student/delete-profile/page.tsx
@@ -1,111 +1,46 @@
 import React from 'react';
 
 
-const StudentSettings = () : React.ReactNode => {
+const StudentDelete = () : React.ReactNode => {
   return (
     <div className="col-xl-9">
-      {/* Privacy START */}
-      <div className="border rounded-3">
-        <div className="row">
-          <div className="col-12">
-            {/* Card START */}
-            <div className="card bg-transparent">
-              {/* Card header START */}
-              <div className="card-header bg-transparent border-bottom">
-                <h3 className="card-header-title">Settings</h3>
-              </div>
-              {/* Card header END */}
-              {/* Card body START */}
-              <div className="card-body">
-                {/* Profile START */}
-                <h5 className="mb-4">Profile Settings</h5>
-                <div className="form-check form-switch form-check-md">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="profilePublic"
-                    checked={true}
-                  />
-                  <label className="form-check-label" htmlFor="profilePublic">
-                    Your profile's public visibility
-                  </label>
-                </div>
-                {/* Profile START */}
-                <hr />
-                {/* Divider */}
-                {/* Notification START */}
-                <h5 className="card-header-title">Notifications Settings</h5>
-                <p className="mb-2 mt-3">
-                  Choose type of notifications you want to receive
-                </p>
-                <div className="form-check form-switch form-check-md mb-3">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="checkPrivacy1"
-                    checked={true}
-                  />
-                  <label className="form-check-label" htmlFor="checkPrivacy1">
-                    Notify me via email when logging in
-                  </label>
-                </div>
-                <div className="form-check form-switch form-check-md mb-3">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="checkPrivacy2"
-                  />
-                  <label className="form-check-label" htmlFor="checkPrivacy2">
-                    Send SMS confirmation for all online payments
-                  </label>
-                </div>
-                <div className="form-check form-switch form-check-md mb-3">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="checkPrivacy3"
-                    checked={true}
-                  />
-                  <label className="form-check-label" htmlFor="checkPrivacy3">
-                    Check which device(s) access your account
-                  </label>
-                </div>
-                <div className="form-check form-switch form-check-md mb-3">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="checkPrivacy4"
-                  />
-                  <label className="form-check-label" htmlFor="checkPrivacy4">
-                    Show your profile publicly
-                  </label>
-                </div>
-                {/* Notification START */}
-                {/* Buttons */}
-                <div className="d-sm-flex justify-content-end">
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-primary me-2 mb-0"
-                  >
-                    Save changes
-                  </button>
-                  <a href="#" className="btn btn-sm btn-outline-secondary mb-0">
-                    Cancel
-                  </a>
-                </div>
-              </div>
-              {/* Card body END */}
-            </div>
-            {/* Card END */}
+      {/* Title and select START */}
+      <div className="card border bg-transparent rounded-3 mb-0">
+        {/* Card header */}
+        <div className="card-header bg-transparent border-bottom">
+          <h3 className="card-header-title mb-0">Deactivate Account</h3>
+        </div>
+        {/* Card body */}
+        <div className="card-body">
+          <h6>Before you go...</h6>
+          <ul>
+            <li>
+              Take a backup of your data <a href="#">Here</a>{" "}
+            </li>
+            <li>If you delete your account, you will lose your all data.</li>
+          </ul>
+          <div className="form-check form-check-md my-4">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              defaultValue=""
+              id="deleteaccountCheck"
+            />
+            <label className="form-check-label" htmlFor="deleteaccountCheck">
+              Yes, I'd like to delete my account
+            </label>
           </div>
-          {/* Privacy END */}
+          <a href="#" className="btn btn-success-soft mb-2 mb-sm-0">
+            Keep my account
+          </a>
+          <a href="#" className="btn btn-danger mb-0">
+            Delete my account
+          </a>
         </div>
       </div>
-      {/* Main content END */}
+      {/* Title and select END */}
     </div>
-
   );
 }
 
-export default StudentSettings ;
+export default StudentDelete ;
